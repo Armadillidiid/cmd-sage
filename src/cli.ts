d52415a86374acdcf3849250724755422956a872
Emmanuel Isenah <emmanuelisenah@gmail.com>
2025-12-08 09:41:13 +0100

    feat: add version

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,12 @@
-import * as Command from "@effect/cli/Command"
+import * as Command from "@effect/cli/Command";
 
-const command = Command.make("cmd-sage")
+declare const __VERSION__: string;
+const VERSION: string =
+  typeof __VERSION__ !== "undefined" ? __VERSION__ : "0.0.0-dev";
+
+const command = Command.make("cmd-sage");
 
 export const run = Command.run(command, {
   name: "cmd-sage",
-  version: "0.0.0"
-})
+  version: VERSION,
+});
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "tsup"
+import { defineConfig } from "tsup";
+import packageJSON from "./package.json" with { type: "json" };
 
 export default defineConfig({
+  define: {
+    __VERSION__: JSON.stringify(packageJSON.version),
+  },
   entry: ["src/bin.ts"],
   clean: true,
   publicDir: true,
   treeshake: "smallest",
-  external: ["@parcel/watcher"]
-})
+  external: ["@parcel/watcher"],
+});

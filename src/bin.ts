0c8231a95235aa7497c0dfc9b82782e6c56ca698
Emmanuel Isenah <emmanuelisenah@gmail.com>
2025-12-09 00:41:47 +0100

    fix: handle 'Ctrl-C` quit exception

--- src/bin.ts
+++ src/bin.ts
@@ -2,7 +2,7 @@
 
 import * as NodeContext from "@effect/platform-node/NodeContext";
 import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
-import { Effect, Layer } from "effect";
+import { Effect, Layer, Console } from "effect";
 import { CliService } from "./cli.js";
 
 const layers = Layer.mergeAll(CliService.Default, NodeContext.layer);
@@ -10,4 +10,11 @@ const layers = Layer.mergeAll(CliService.Default, NodeContext.layer);
 Effect.gen(function* () {
   const cli = yield* CliService;
   yield* cli.run(process.argv);
-}).pipe(Effect.provide(layers), NodeRuntime.runMain());
+}).pipe(
+  Effect.catchTags({
+    QuitException: () =>
+      Console.log("\n\n❌ Operation cancelled by user.\n").pipe(Effect.asVoid),
+  }),
+  Effect.provide(layers),
+  NodeRuntime.runMain(),
+);
